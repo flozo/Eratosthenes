d9a1a9456cb4c597958769a576d38ca63fd2ddaa
flozo.home <github.mail@flozo.de>
2022-04-05 12:45:36 +0200

    Add hours to sifting time output

--- src/de/flozo/Main.java
+++ src/de/flozo/Main.java
@@ -5,15 +5,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int limit = 10000000;
+        int limit = 100_000_000;
         // Save timestamp at start
         long startTime = System.nanoTime();
         printList(limit);
         // Save timestamp at the end
         long endTime = System.nanoTime();
-        String durationString = duration((endTime - startTime) / 1000000);
-        System.out.println("It took " + durationString);
+        long milliseconds = (endTime - startTime) / 1000000;
+        String durationString = duration(milliseconds);
+        System.out.println("Sifting time was " + milliseconds + " ms = " + durationString + " (hh:mm:ss.f)");
 
+//        System.out.println(duration(8128248));
     }
 
     public static void printList(int limit) {
@@ -32,9 +34,10 @@ public class Main {
     public static String duration(long milliseconds) {
         int ms = (int) milliseconds % 1000;
         int s = (int) (milliseconds / 1000) % 60;
-        int m = (int) milliseconds / 60000;
+        int m = (int) (milliseconds / 60000) % 60;
+        int h = (int) milliseconds / 3600000;
         System.out.println(s);
-        return String.format("%02d:%02d.%02d", m, s, ms);
+        return String.format("%02d:%02d:%02d.%02d", h, m, s, ms);
     }
 
 }
